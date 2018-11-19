1e267a856eca7b6e83079eadb1610197ec5481c1
Mathieu SOUM <mathieu.soum@gmail.com>
2018-11-19 22:49:44 +0100

    GET and POST moved to AlarmListApi. New GET and DELETE for AlarmApi (single alarm management).

--- alarm.py
+++ alarm.py
@@ -1,6 +1,6 @@
 import time
 import json
-from flask_restful import reqparse, Resource
+from flask_restful import reqparse, abort, Resource
 
 alarm_list = []
 
@@ -16,7 +16,7 @@ alarm_parser.add_argument('days')
 alarm_parser.add_argument('active')
 
 
-class AlarmApi(Resource):
+class AlarmListApi(Resource):
     def get(self):
         return {'alarm_list': [alarm.json_dict() for alarm in alarm_list]}
 
@@ -25,6 +25,36 @@ class AlarmApi(Resource):
         new_alarm = Alarm(args['time'], args['days'], args['active'])
         alarm_list.append(new_alarm)
         return new_alarm.json_dict()
+
+
+def abort_if_alarm_doesnt_exists(alarm_id):
+    if alarm_id not in [alarm.id for alarm in alarm_list]:
+        abort(404, message="Alarm {} does not exists".format(alarm_id))
+
+
+def get_alarm_from_list(alarm_id):
+    result = [alarm for alarm in alarm_list if alarm.id == alarm_id]
+    if result:
+        return result[0]
+    else:
+        return None
+
+
+class AlarmApi(Resource):
+    def get(self, alarm_id):
+        abort_if_alarm_doesnt_exists(alarm_id)
+        return [alarm for alarm in alarm_list if alarm.id == alarm_id][0].json_dict()
+
+    def delete(self, alarm_id):
+        global alarm_list
+        abort_if_alarm_doesnt_exists(alarm_id)
+        get_alarm_from_list(alarm_id).active = False  # Deactivates the alarm
+        alarm_list = [alarm for alarm in alarm_list if alarm.id != alarm_id]
+        return '', 204
+
+    def put(self, alarm_id):
+        # TODO Change 'time', 'days' or 'active' regarding what parameters were given
+        pass
 
 
 class Alarm:
@@ -56,14 +86,36 @@ class Alarm:
                     self.days[key] = json_days[key]
 
         if _active is None:
-            self.active = False
+            self.__active = False
         else:
-            self.active = bool(_active)
+            self.__active = bool(_active)
+
+    @property
+    def active(self):
+        return self.__active
+
+    @active.setter
+    def active(self, value):
+        prev = self.__active
+        self.__active = value
+
+        if not prev and self.__active:  # Rising edge detection to avoid multiple cron interactions
+            self.register_alarm()
+        elif prev and not self.__active:  # Falling edge detection to avoid multiple cron interactions
+            self.unregister_alarm()
+
+    def register_alarm(self):
+        # TODO Register alarm as a cron script
+        pass
+
+    def unregister_alarm(self):
+        # TODO Unregister alarm as a cron script
+        pass
 
     def json_dict(self):
         return {
             'id':     self.id,
             'time':   self.time,
             'days':   self.days,
-            'active': self.active
+            'active': self.__active
         }
--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_restful import Api
-from alarm import AlarmApi, Clock
+from alarm import AlarmApi, AlarmListApi, Clock
 
 app = Flask(__name__)
 api = Api(app)
 
 
 api.add_resource(Clock, '/clock')
-api.add_resource(AlarmApi, '/alarm')
+api.add_resource(AlarmListApi, '/alarms')
+api.add_resource(AlarmApi, '/alarm/<int:alarm_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
